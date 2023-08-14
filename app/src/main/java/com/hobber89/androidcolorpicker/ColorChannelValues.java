ef679e9ba6f671419efcb8827c1427e0110999be
hobber89 <dotteldu@gmail.com>
2023-08-14 19:58:27 +0200

    MainActivity: show current color in a StackView.

---
+++ app/src/main/java/com/hobber89/androidcolorpicker/ColorChannelValues.java
@@ -0,0 +1,43 @@
+package com.hobber89.androidcolorpicker;
+
+public class ColorChannelValues {
+    private short r;
+    private short g;
+    private short b;
+
+    public ColorChannelValues() {
+        r = 0;
+        g = 0;
+        b = 0;
+    }
+
+    public short getR() {
+        return r;
+    }
+
+    public void setR(short r) {
+        this.r = r;
+    }
+
+    public short getG() {
+        return g;
+    }
+
+    public void setG(short g) {
+        this.g = g;
+    }
+
+    public short getB() {
+        return b;
+    }
+
+    public void setB(short b) {
+        this.b = b;
+    }
+
+    public int getColor() {
+        int a = 255;
+        int color = (b & 0xff) + ((g & 0xff) << 8) + ((r  & 0xff) << 16) + (a << 24);
+        return color;
+    }
+}
--- app/src/main/java/com/hobber89/androidcolorpicker/MainActivity.java
+++ app/src/main/java/com/hobber89/androidcolorpicker/MainActivity.java
@@ -3,12 +3,21 @@ package com.hobber89.androidcolorpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.StackView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ColorChannelValues currentColor;
+    private StackView currentColorStackView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentColor = new ColorChannelValues();
+
+        currentColorStackView = findViewById(R.id.currentColorStackView);
+        currentColorStackView.setBackgroundColor(currentColor.getColor());
     }
 }
